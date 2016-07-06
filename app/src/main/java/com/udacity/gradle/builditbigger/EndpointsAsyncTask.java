61b957a6ab6a79e692bc03bd199dc3539e4e99d7
aman <amandalmia18@gmail.com>
2016-07-06 13:32:02 +0530

    Adding gradle tasks for local server

--- app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -8,6 +8,8 @@ import com.example.aman.myapplication.backend.myApi.MyApi;
 import com.example.displayjoke.DisplayJokeActivity;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 
 import java.io.IOException;
 
@@ -21,9 +23,19 @@ public class EndpointsAsyncTask extends AsyncTask<Context, Void, String> {
     @Override
     protected String doInBackground(Context... params) {
         if (myApiService == null) {  // Only do this once
-            MyApi.Builder builder = new MyApi.Builder(
-                    AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
-                    .setRootUrl("https://build-it-bigger-1362.appspot.com/_ah/api/");
+
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    // options for running against local devappserver
+                    // - 10.0.2.2 is localhost's IP address in Android emulator
+                    // - turn off compression when running against local devappserver
+                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                            abstractGoogleClientRequest.setDisableGZipContent(true);
+                        }
+                    });
             // end options for devappserver
 
             myApiService = builder.build();
